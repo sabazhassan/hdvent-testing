d542225863b92d9233162c18f2afd94e3df202e3
dgrims <grimshandl@physi.uni-heidelberg.de>
2020-04-02 13:01:24 +0200

    changed to c++ naming convention

--- X-NUCLEO-IHM02A1/test-positioning/test-positioning.ino
+++ X-NUCLEO-IHM02A1/test-positioning/test-positioning.ino
@@ -25,37 +25,42 @@ AutoDriver *boardIndex ;
 
 unsigned int speed = 200;
 byte spi_test();
-void configureBoards();
+void ConfigureBoards();
 int getBoardConfigurations();
 int getBoardStatus();
-void singleMotorCycle();
-void updateMotorCurveParameters(float respiratory_rate, float path_ratio, float ie_ratio);
-void printMotorCurveParameters();
-bool is_busy();
+void SingleMotorCycle();
+void UpdateMotorCurveParameters(float respiratoryRate, float pathRatio, float IERatio);
+void PrintMotorCurveParameters();
+bool isBusy();
 
 // motor curve for volume control operation
 // inspiratory phase
-float acc_in = 300; // steps/s/s
-float speed_in = 100; // steps/s
-float dec_in = 300; // steps/s/s
-float t_in=1;
-// hold time after inspiratory phase
-float time_hold_plateau = 0.200; // seconds
+float const ACC_IN = 200; // steps/s/s
+float speedIn = 100; // steps/s
+float const DEC_IN = 200; // steps/s/s
+float timeIn=1;
 
-// expiratory phase
-float acc_ex = 300; // steps/s/s
-float speed_ex = 100; //steps/s
-float dec_ex = 300; // steps/s/s
-float t_ex=1;
+// hold time after inspiratory phase
+float const TIME_HOLD_PLATEAU = 0.200; // seconds
+int const stepDivider = STEP_FS_4;
+
+
+// exspiratory phase
+float const ACC_EX = 200; // steps/s/s
+float const SPEED_EX = 100; //steps/s
+float const DEC_EX = 200; // steps/s/s
+float timeEx=1;
 
 //float time_hold_ex = 4; // seconds
-unsigned int steps_full_range = 100;
-unsigned int steps_interval = 100;
+
+//
+unsigned int stepsFullRange = 100*4;
+unsigned int stepsInterval = 100*4;
 
 // user-set parameters (later by poti)
-float respiratory_rate = 15;
-float path_ratio = 0.7;
-float ie_ratio = 0.5;
+float respiratoryRate = 15;
+float pathRatio = 0.7;
+float IERatio = 0.5;
 
 void setup()
 {
@@ -79,74 +84,80 @@ void setup()
     //  for them. See below for more information about the
     //  settings applied.
     spi_test();
-    configureBoards();
+    ConfigureBoards();
 
 }
+
 
 void loop(){
-    updateMotorCurveParameters(respiratory_rate, path_ratio, ie_ratio);
-    printMotorCurveParameters();
-    singleMotorCycle();
+    UpdateMotorCurveParameters(respiratoryRate, pathRatio, IERatio);
+    //Stepper.move(0,1);
+    //delay(5000);
+    PrintMotorCurveParameters();
+    SingleMotorCycle();
+    getBoardStatus();
 }
+
 
 //! take user-set parameters and calculate the parameters for the motor curve,
 //!
-//! \param respiratory_rate - respiratory rate in breaths per minute
-//! \param path_ratio - float between 0-1, setting the portion of the total path that will be driven
-//! \param ie_ratio - (inhalation time)/(exhalation time)
-void updateMotorCurveParameters(float respiratory_rate, float path_ratio, float ie_ratio) {
-    float t_total = 60 / respiratory_rate;
-    t_ex = t_total / (ie_ratio + 1);
-    t_in = t_total - t_ex ;
-    int steps = steps_full_range * path_ratio;
-    //Serial.print("t_in: "); Serial.println(t_in);
-    //Serial.print("t_ex: "); Serial.println(t_ex);
+//! \param respiratoryRate - respiratory rate in breaths per minute
+//! \param pathRatio - float between 0-1, setting the portion of the total path that will be driven
+//! \param IERatio - (inhalation time)/(exhalation time)
+void UpdateMotorCurveParameters(float respiratoryRate, float pathRatio, float IERatio) {
+    float t_total = 60 / respiratoryRate;
+    timeEx = t_total / (IERatio + 1);
+    timeIn = t_total - timeEx ;
+    int steps = stepsFullRange * pathRatio;
+    //Serial.print("timeIn: "); Serial.println(timeIn);
+    //Serial.print("timeEx: "); Serial.println(timeEx);
 
-    float discriminant = sq(t_in) - 2 * (1. / acc_in + 1. / dec_in) * steps;
+    float discriminant = sq(timeIn) - 2 * (1. / ACC_IN + 1. / DEC_IN) * steps;
     //Serial.print("discriminant: "); Serial.println(discriminant);
     if (discriminant > 0) {
-        speed_in = (-t_in + sqrtf(discriminant)) / -(1. / acc_in + 1. / dec_in);
-        //time_hold_ex =  t_ex - ((1. / acc_ex + 1. / dec_ex)*sq(speed_ex)/2 + steps)/speed_ex;
-        steps_interval = steps;
+        speedIn = (-timeIn + sqrtf(discriminant)) / -(1. / ACC_IN + 1. / DEC_IN);
+        //time_hold_ex =  timeEx - ((1. / ACC_EX + 1. / DEC_EX)*sq(SPEED_EX)/2 + steps)/SPEED_EX;
+        stepsInterval = steps;
     }
 }
 
-void printMotorCurveParameters(){
-    Serial.print("speed_in: "); Serial.println(speed_in);
+void PrintMotorCurveParameters(){
+    Serial.print("speedIn: "); Serial.println(speedIn);
     //Serial.print("time_hold_ex "); Serial.println(time_hold_ex);
-    Serial.print("steps_interval: "); Serial.println(steps_interval);
+    Serial.print("stepsInterval: "); Serial.println(stepsInterval);
 }
 
-void singleMotorCycle(){
-    boardIndex->setMaxSpeed(speed_in);
-    boardIndex->setAcc(acc_in);
-    boardIndex->setDec(dec_in);
+void SingleMotorCycle(){
+    boardIndex->setMaxSpeed(speedIn);
+    boardIndex->setAcc(ACC_IN);
+    boardIndex->setDec(DEC_IN);
 
-    Stepper.move(0,steps_interval);
 
-    // ensure inspiration phase lasts t_in seconds
+    Stepper.move(0,stepsInterval);
+
+    // ensure inspiration phase lasts timeIn seconds
     unsigned long startTime = millis();
     unsigned long elapsedTime = millis()-startTime;
-    while( is_busy() || elapsedTime < (t_in-time_hold_plateau)*1000){
+    while( isBusy() || elapsedTime < (timeIn-TIME_HOLD_PLATEAU)*1000){
         elapsedTime = millis()-startTime;
     };
-    delay(1000*time_hold_plateau);
+    delay(1000*TIME_HOLD_PLATEAU);
 
-    boardIndex->setMaxSpeed(speed_ex);
-    boardIndex->setAcc(acc_ex);
-    boardIndex->setDec(dec_ex);
-    Stepper.move(1, steps_interval);
+    boardIndex->setMaxSpeed(SPEED_EX);
+    boardIndex->setAcc(ACC_EX);
+    boardIndex->setDec(DEC_EX);
+    Stepper.move(1, stepsInterval);
 
-    // ensure exspiration phase lasts t_ex seconds
+    // ensure exspiration phase lasts timeEx seconds
     startTime = millis();
     elapsedTime = millis()-startTime;
-    while( is_busy() || elapsedTime < t_ex*1000){
+    while( isBusy() || elapsedTime < timeEx*1000){
         elapsedTime = millis()-startTime;
     };
 
 }
 
-bool is_busy()
+bool isBusy()
 {
     byte status=0;
     bool busy=0;
@@ -177,7 +188,7 @@ byte spi_test()
 
 
 //  For ease of reading, we're just going to configure all the boards to the same settings.
-void configureBoards()
+void ConfigureBoards()
 {
     int paramValue;
     Serial.println("Configuring boards...");
@@ -197,14 +208,14 @@ void configureBoards()
     //  This lets us hammer the motor harder during some phases  than others, and to use a higher voltage to achieve better
     //  torqure performance even if a motor isn't rated for such a high current.
     // This IHM02A1 BOARD has 12V motors and a 12V supply.
-    boardIndex->setRunKVAL(140);  // 128/255 * 12V = 6V
-    boardIndex->setAccKVAL(160);  // 192/255 * 12V = 6V
-    boardIndex->setDecKVAL(140);  // 128/255 * 12V = 3V
-    boardIndex->setHoldKVAL(80);  // 32/255 * 12V = 1.5V  // low voltage, almost free turn
+    boardIndex->setRunKVAL(240);  // 220/255 * 12V = 6V
+    boardIndex->setAccKVAL(240);  // 220/255 * 12V = 6V
+    boardIndex->setDecKVAL(240);  // /255 * 12V = 3V
+    boardIndex->setHoldKVAL(180);  // 132/255 * 12V = 1.5V  // low voltage, almost free turn
 
     // The dSPIN chip supports microstepping for a smoother ride. This function provides an easy front end for changing the microstepping mode.
     // once in full speed, it will step up to half-step
-    boardIndex->configStepMode(STEP_FS); // Full step
+    boardIndex->configStepMode(stepDivider); // Full step
 
     // When a move command is issued, max speed is the speed the Motor tops out at while completing the move, in steps/s
     boardIndex->setMaxSpeed(100);
@@ -217,5 +228,37 @@ void configureBoards()
 
 
 
+}
+
+int getBoardStatus()
+{
+    int paramValue;
+    Serial.print("\n Status ");
+
+    // See Datasheet page 55 : Status register
+    // MOT_STAT: 00 = stopped / 01=Acc  / 10=Dec / 11 = const speed
+    // OCD = Over current Flag (Active low)
+    paramValue = boardIndex->getStatus();
+    Serial.print("[hex]"); Serial.println(paramValue, HEX);
+    Serial.println("0-HIZ | 1-BUSY | 2-SW_F | 3-SW_EVN | 4-DIR |  5-MOT_STAT  | NOPR_CMD");
+    Serial.print("   ");Serial.print(paramValue&0x01);
+    Serial.print("  |  "); Serial.print((paramValue>>1)&0x01);
+    Serial.print("     |  "); Serial.print((paramValue>>2)&0x01);
+    Serial.print("     |  "); Serial.print((paramValue>>3)&0x01);
+    Serial.print("       |  "); Serial.print((paramValue>>4)&0x01);
+    Serial.print("    |    "); Serial.print((paramValue>>5)&0x03);
+    Serial.print("         |  "); Serial.println((paramValue>>7)&0x01);
+    Serial.println("-------------------------------------------------------------------");
+    Serial.println("8-WRNG_CMD | 9-UVLO | 10-TH_WRN | 11-TH_SD | 12-OCD | 13-STLOS_A | 14-STLOS_B | 15-SCK_MOD");
+    Serial.print("   "); Serial.print((paramValue>>8)&0x01);
+    Serial.print("       |  "); Serial.print((paramValue>>9)&0x01);
+    Serial.print("     |  "); Serial.print((paramValue>>10)&0x01);
+    Serial.print("        |  "); Serial.print((paramValue>>11)&0x01);
+    Serial.print("       |  "); Serial.print((paramValue>>12)&0x01);
+    Serial.print("     |  "); Serial.print((paramValue>>13)&0x01);
+    Serial.print("         |  "); Serial.print((paramValue>>14)&0x01);
+    Serial.print("         |  "); Serial.println((paramValue>>15)&0x01);
+
+    return(paramValue);
 }
 
